f46da80ad1269dd5f5872ca34c975c8159cc8c6e
Fr-ocasting <romainrochas98@gmail.com>
2024-06-25 09:53:34 +0200

    commit

--- load_netmob_data.py
+++ load_netmob_data.py
@@ -7,6 +7,112 @@ import glob
 from shapely.geometry import Point,Polygon
 from shapely.errors import ShapelyDeprecationWarning
 import warnings 
+import torch
+import torch.nn.functional as F
+from datetime import datetime,timedelta
+
+
+def calculate_grid_size(tile_ids,epsilon):
+    ''' Calculate Image dimension. Simple function cause Cell-size = 100m*100m '''
+    # Remove tile ids '-1' 
+    tile_ids_real =tile_ids[tile_ids> 0]
+    
+    H = W = 2*(epsilon//100 + 1)   # +1 to be sure. 2* cause epsilon is the radius 
+    
+    # === ... 
+    return(H,W,tile_ids_real)
+
+def get_grid(tile_ids_real,H,W,step_south_north = 287):
+    ''' Lower-left corner (South - West) is supposed to be the minimal tile-id '''
+    init_mini = tile_ids_real.min()
+    mini = init_mini
+    find = False
+
+    # find 'mini', the lower_left_corner: 
+    while not find:
+    
+        init_grid = np.array([[(mini + w + step_south_north*h) for w in range(W)]for h in range(H)])
+        condition = set(tile_ids_real).issubset(set(init_grid.reshape(-1)))
+
+        if condition:
+            find = True
+        else:
+            mini = mini-1
+
+        if mini < init_mini- 287:
+            raise ValueError("Init Grid can't match Tile-Ids. There is an error within code.")
+
+    return(init_grid)
+
+def match_tile_ids_with_grid(v1,v2):    
+    positions = [np.where(v2 == x)[0][0] for x in v1]
+    return(positions)
+
+
+def resize_tensor(T_i, H,W, positions):
+    N,C,L = T_i.shape
+    new_L = H*W
+    new_T_i = torch.zeros((N,C,new_L))
+    
+    for idx,pos in enumerate(positions):
+        new_T_i[:,:,pos] = T_i[:,:,idx]
+        
+    return(new_T_i)
+
+
+def read_csv(path,result,metadata,day,maxi_nb_tile):
+    transfer_mode = path.split('.')[-2].split('_')[-1]
+    day_str = str(day)
+    day_str = datetime.strptime(day_str, '%Y%m%d')
+    times = [day_str + timedelta(minutes=15*i) for i in range(96)]
+    times_str = [t.strftime('%H:%M') for t in times]
+    columns = ['tile_id'] + times_str
+
+    Tensors = []
+    df = pd.read_csv(path, sep = ' ', names = columns).set_index(['tile_id'])
+    # Loop through each station. Get associated usefull Tile-id
+    for station_ind in range(len(result)):
+        station = result['COD_TRG'][station_ind]
+        ids = result.tile_id[station_ind]
+        #T =  torch.Tensor(df.loc[ids].values, dtype = torch.int32) 
+        T =  torch.tensor(df.loc[ids].values, dtype = torch.int32)
+        padding = maxi_nb_tile-T.size(0)
+
+        if padding > 0:
+            T = F.pad(T,(0,0,0,padding),'constant',0)
+            ids = ids+[-1]*padding
+
+        Tensors.append(T)
+        metadata[station]['tile_id'] = ids
+    Tensors = torch.stack(Tensors,dim = 0)
+
+    return(Tensors,transfer_mode,metadata)
+
+def tackle_all_days(result,metadata,netmob_data_folder_path,app,maxi_nb_tile,folder_days):
+    # for each days  
+    Tensors_days = []
+    for day in folder_days:
+        Tensors_days,metadata = tackl_one_day(result,metadata,netmob_data_folder_path,app,day,Tensors_days,maxi_nb_tile)
+    Tensors_days = torch.stack(Tensors_days,dim=0)
+    return(Tensors_days,metadata)
+
+def tackl_one_day(result,metadata,netmob_data_folder_path,app,day,Tensors_days,maxi_nb_tile):
+    txt_paths = sorted(glob.glob(os.path.join(f'{netmob_data_folder_path}/{app}/{day}', "*.txt")))
+    # For each transfert mode:
+    Tensors_transfer,transfer_modes = [],[]
+    for path in txt_paths:
+        Tensors,transfer_mode,metadata = read_csv(path,result,metadata,day,maxi_nb_tile)
+        Tensors_transfer.append(Tensors)
+        transfer_modes.append(transfer_mode)
+    Tensors_transfer = torch.stack(Tensors_transfer,dim=0) 
+
+
+    # Update metadata
+    for station in result['COD_TRG']:
+        metadata[station]['mode_transfer'] = transfer_modes
+    Tensors_days.append(Tensors_transfer)
+    return(Tensors_days,metadata)
+
 
 def find_ids_within_epsilon(gdf1,gdf2,epsilon):
     gdf1 = gdf1.to_crs(epsg=2154)
@@ -64,7 +170,7 @@ def restrain_netmob_to_Lyon(Netmob_gdf,folder_path,zones_path):
     Netmob_gdf = gpd.GeoDataFrame(Netmob_gdf)
     Netmob_gdf.crs = 'epsg:4326'
     
-    restrained_Lyon_gdf = gpd.sjoin(Netmob_gdf,working_zones,how = 'inner')
+    restrained_Lyon_gdf = gpd.sjoin(Netmob_gdf,working_zones,how = 'inner') #how = 'left')
     return(restrained_Lyon_gdf,working_zones)
 
 
@@ -73,7 +179,7 @@ def load_netmob_restrained_to_lyon(folder_path,data_folder,
                     zones_path = 'lyon_iris_shapefile/lyon.shp',
                     ):
 
-    Netmob_gdf,n_rows,n_cols = load_netmob_json(folder_path, geojson_path = geojson_path)
+    Netmob_gdf,n_rows,n_cols = load_netmob_json(folder_path, geojson_path)
     Netmob_gdf['centroid_lonlat'] = Netmob_gdf.geometry.apply(lambda x : x.centroid)
     Netmob_gdf = Netmob_gdf.set_geometry('centroid_lonlat')
     Netmob_gdf_joined,working_zones = restrain_netmob_to_Lyon(Netmob_gdf,data_folder,zones_path) #Associate an square to an IRIS when the centroid is inside it
@@ -82,15 +188,8 @@ def load_netmob_restrained_to_lyon(folder_path,data_folder,
 
 def load_netmob_gdf(folder_path = '../../Data/NetMob/',data_folder = '../../Data/lyon_iris_shapefile/', geojson_path = 'Lyon.geojson',zones_path = 'lyon.shp'):
     Netmob_gdf_joined,working_zones = load_netmob_restrained_to_lyon(folder_path,data_folder,geojson_path,zones_path)
-    Netmob_gdf_joined = Netmob_gdf_joined[['tile_id','INSEE_COM','NOM_COM','NOM_IRIS','geometry']]
+    Netmob_gdf_joined = Netmob_gdf_joined[['tile_id','INSEE_COM','NOM_COM','NOM_IRIS','geometry']] # 
     Netmob_gdf_joined = gpd.GeoDataFrame(Netmob_gdf_joined,crs='EPSG:4326') #Don't know why, but the geodataframe seem corrupted as we can't convert it into GeoJson, that's why we need to use "gpd.GeoDataFrame()"
 
-    return(Netmob_gdf_joined)
+    return(Netmob_gdf_joined,working_zones)
 
-
-
-def get_netmob_csv_paths(folder_path = '../../Data/NetMob/' ):
-    apps = [app for app in listdir(folder_path) if ((app != 'Lyon.geojson') and (not app.startswith('.'))) ]   # Avoid hidden folder and Lyon.geojson
-    folder_days = [[day for day in listdir(f'{folder_path}/{app}') if (not day.startswith('.')) ] for app in apps]
-    day = folder_days[0]
-    txt_paths = sorted(glob.glob(os.path.join(f'{folder_path}/{app}/{day}', "*.txt")))
