158ef1c5a8ed276c5d60da62283c8a20a5cc2838
Fr-ocasting <romainrochas98@gmail.com>
2024-08-20 19:30:53 +0200

    add HP tuning .py example

--- examples/HP_parameter_choice.py
+++ examples/HP_parameter_choice.py
@@ -1,23 +1,24 @@
 import pandas as pd  # if not, I get this error while running a .py from terminal: 
 # ImportError: /lib64/libstdc++.so.6: version `CXXABI_1.3.9' not found (required by /root/anaconda3/envs/pytorch-2.0.1_py-3.10.5/lib/python3.10/site-packages/pandas/_libs/window/aggregations.cpython-310-x86_64-linux-gnu.so)
 
-
 # Relative path:
 import sys 
 import os 
-current_file_path = os.path.abspath(os.path.dirname(__file__))
-parent_dir = os.path.abspath(os.path.join(current_file_path,'..'))
+current_path = os.path.abspath(os.path.dirname(__file__))
+working_dir = os.path.abspath(os.path.join(current_path,'..'))
 #parent_dir = f"{parent_dir}/prediction_validation/"  # A utiliser sur .ipynb notebook
-if parent_dir not in sys.path:
-    sys.path.insert(0,parent_dir)
+if working_dir not in sys.path:
+    sys.path.insert(0,working_dir)
 # ...
 
-
-from constants.paths import folder_path,file_name
-from constants.config import get_args,update_modif
-from high_level_DL_method import evaluate_config 
+# Personnal import 
 from utils.utilities_DL import match_period_coverage_with_netmob,get_small_ds
+from constants.config import get_args,update_modif
+from constants.paths import folder_path,file_name,SAVE_DIRECTORY
+from K_fold_validation.K_fold_validation import KFoldSplitter
 
+# Hp Tuning
+from HP_tuning.hyperparameter_tuning_ray import HP_tuning
 
 # Load config
 model_name = 'STGCN' #'CNN'
@@ -26,11 +27,9 @@ args = get_args(model_name)
 # Modification : 
 args.K_fold = 5
 args.ray = True
-
-args.epochs = 1
+args.W = 0  # IMPORTANT AVEC NETMOB
+args.epochs = 100
 args.loss_function_type = 'MSE' # 'quantile'
-args.mixed_precision = False
-args.torch_compile = False
 
 args = update_modif(args)
 
@@ -40,9 +39,19 @@ coverage = match_period_coverage_with_netmob(file_name)
 (coverage,args) = get_small_ds(small_ds,coverage,args)
 
 # Choose DataSet and VisionModel if needed: 
-dataset_names = ['netmob'] # ['calendar','netmob'] #['subway_in','netmob','calendar']
-vision_model_name = 'ImageAvgPooling'  # 'ImageAvgPooling'  #'FeatureExtractor_ResNetInspired' #'MinimalFeatureExtractor',
+dataset_names = ['netmob','subway_in'] # ['calendar','netmob'] #['subway_in','netmob','calendar']
+vision_model_name = 'FeatureExtractor_ResNetInspired'  # 'ImageAvgPooling'  #'FeatureExtractor_ResNetInspired' #'MinimalFeatureExtractor',
 
-# Train and Evaluate Model: 
-mod_plot = 1 # bokeh plotting every epoch 
-trainer,model,args,pi,pi_cqr = evaluate_config(dataset_names,folder_path,file_name,args,coverage,vision_model_name,mod_plot)
+# === Train and Evaluate Model: 
+
+# Load K-fold subway-ds 
+folds = [0] # Here we use the first fold for HP-tuning. In case we need to compute the Sliding K-fold validation: folds = np.arange(1,args.K_fold)
+
+# Split in K-fold : 
+K_fold_splitter = KFoldSplitter(dataset_names,args,coverage,folder_path,file_name,vision_model_name,folds)
+K_subway_ds,args_embedding,args_vision,dic_class2rpz = K_fold_splitter.split_k_fold()
+
+# Train on the first fold: 
+num_samples = 100
+subway_ds = K_subway_ds[0]
+analysis = HP_tuning(subway_ds,args,args_embedding,args_vision,num_samples,dic_class2rpz,working_dir)
