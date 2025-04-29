5c4d6db1521b72ce127ec5de63e03cae1518c478
Fr-ocasting <romainrochas98@gmail.com>
2025-04-29 11:19:36 +0200

    start dow et tod

--- build_inputs/load_calendar.py
+++ build_inputs/load_calendar.py
@@ -44,6 +44,11 @@ def load_calendar(subway_ds):
         dict_calendar_U_valid[calendar_type] =  valid_tensor_ds.tensor if valid_tensor_ds is not None else None
         dict_calendar_U_test[calendar_type] = test_tensor_ds.tensor if test_tensor_ds is not None else None
 
+    AJOUTER LE CALENDAR DAY OF WEEK ET TIME OF DAY
+
+    for calendar_type in ['dow','tod']:
+        BLABLBA
+
     return(dict_calendar_U_train,dict_calendar_U_valid,dict_calendar_U_test)
 
 def dim_by_calendar_type(calendar_type):
@@ -75,55 +80,3 @@ def get_args_embedding(args,dict_calendar_U_train):
     args.args_embedding = args_embedding
 
     return(args)
-
-"""
-def load_calendar(subway_ds):
-    '''Tackling Calendar Data''' 
-    Dic_T_labels,Dic_class2rpz,Dic_rpz2class,Dic_nb_words_embedding = get_time_slots_labels(subway_ds)
-    tensor_limits_keeper = subway_ds.tensor_limits_keeper
-
-    dict_calendar_U_train,dict_calendar_U_valid,dict_calendar_U_test = {},{},{}
-    for calendar_class in [0,1,2,3]:
-        calendar_tensor = Dic_T_labels[calendar_class] #args.calendar_class
-
-        splitter = TrainValidTest_Split_Normalize(calendar_tensor,
-                                    first_train = tensor_limits_keeper.first_train_U, last_train= tensor_limits_keeper.last_train_U,
-                                    first_valid= tensor_limits_keeper.first_valid_U, last_valid = tensor_limits_keeper.last_valid_U,
-                                    first_test = tensor_limits_keeper.first_test_U, last_test = tensor_limits_keeper.last_test_U)
-
-        train_tensor_ds,valid_tensor_ds,test_tensor_ds = splitter.split_normalize_tensor_datasets(normalizer = None)
-
-        dict_calendar_U_train[calendar_class] = train_tensor_ds.tensor
-        dict_calendar_U_valid[calendar_class] =  valid_tensor_ds.tensor if valid_tensor_ds is not None else None
-        dict_calendar_U_test[calendar_class] = test_tensor_ds.tensor if test_tensor_ds is not None else None
-    return(dict_calendar_U_train,dict_calendar_U_valid,dict_calendar_U_test,Dic_class2rpz,Dic_rpz2class,Dic_nb_words_embedding)
-
-def get_args_embedding(args,nb_words_embedding,dic_class2rpz):
-    if 'calendar' in args.dataset_names:
-        module_path = f"dl_models.TimeEmbedding.load_config"
-        module = importlib.import_module(module_path)
-        importlib.reload(module)
-        args_embedding = module.args
-        args_embedding.nb_words_embedding = nb_words_embedding
-        args_embedding.dic_class2rpz = dic_class2rpz
-    else:
-        args_embedding = argparse.ArgumentParser(description='TimeEmbedding').parse_args(args=[])
-
-    args.args_embedding = args_embedding
-
-    return(args)
-
-
-def tackle_calendar(args,dic_class2rpz,dic_rpz2class,nb_words_embedding):
-    if not('calendar' in args.dataset_names):
-        dic_class2rpz,dic_rpz2class,nb_words_embedding = None,None,None
-        
-    args = get_args_embedding(args,nb_words_embedding,dic_class2rpz)
-
-    args.dic_class2rpz = dic_class2rpz
-    args.dic_rpz2class = dic_rpz2class
-    args.nb_words_embedding = nb_words_embedding
-
-
-    return args
-"""
