c7683f3a3348134788b650300d007f829b4532d9
Fr-ocasting <romainrochas98@gmail.com>
2026-02-06 18:24:25 +0100

    update

---
+++ experiences/pipeline_desag/full_pipeline_citibike.py
@@ -0,0 +1,304 @@
+# ==================================================
+# IMPORT
+import re 
+import os 
+import pandas as pd
+import sys
+import torch 
+import importlib
+import itertools 
+import copy 
+import torch._dynamo as dynamo; dynamo.graph_break()
+torch._dynamo.config.verbose=True
+
+if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32  = True
+
+current_file_path = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(os.path.join(current_file_path,'..','..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0,parent_dir)
+
+from pipeline.utils.loger import LOG
+from experiences.common_parameter import possible_target_kwargs, weather_possible_contextual_kwargs, model_configurations,feature_extractor_model_configurations
+from experiences.common_parameter import REPEAT_TRIAL,netmob_preprocessing_kwargs
+from experiences.get_desagregated_plot import get_desagregated_gains
+from experiences.compilation_parameter import compilation_modification
+from experiences.loop_train_save_log import loop_train_save_log
+from experiences.pipeline_desag.build_config_single_contextual import ConfigBuilder
+from experiences.pipeline_desag.build_baseline_config import BaselineConfigBuilder
+from experiences.pipeline_desag.utils import plotting_boxplot_of_trials
+from experiences.pipeline_desag.MetricExporter import MetricExporter
+from examples.accuracy_comparison import get_cluster
+from examples.train_model import load_init_model_trainer_ds
+from examples.benchmark import local_get_args
+from constants.paths import ROOT
+inside_saved_folder = 'K_fold_validation/training_wo_HP_tuning'
+folder_path = f"{ROOT}/save/{inside_saved_folder}"
+
+# ==================================================
+# SET PARAMETERS OF THE EXPERIMENT AND LOGER: 
+'''
+- Do not set seed to have different initialization for each trial
+- SANITY_CHECKER: If True, Keep track of the gradients and the weights during the training to detect possible problems.
+'''
+
+# ------------------------------------------
+exp_i = 'pipeline_citibike_out'
+freq = '1H' #'15min'  
+horizons = [1,4] # [4]  #[1,4]
+model_name = 'STAEformer'
+target_data = 'citibike_out' 
+dataset_for_coverage = ['citibike_out','MTA_in'] 
+possible_contextual_dataset_names = [
+                                    # ['bike_in'],
+                                    ['MTA_in'],
+                                    # ['citibike_in'],
+                                    # ['subway_in_subway_out','bike_in','weather'],
+                                    #  ['subway_in_subway_out','bike_in'],
+                                    #  ['weather'],
+                                    #  ['weather','bike_in'],
+                                    #  ['weather','subway_in_subway_out'],
+                                     ] # ['netmob_POIs'] #['subway_out']
+TRIVIAL_TEST = False
+REPEAT_TRIAL = 5 
+THRESHOLD_VOLUME_MIN = 5
+possible_target_kwargs[target_data]['threshold_volume_min'] = THRESHOLD_VOLUME_MIN  # number_of_month_in_period
+
+config_backbone_model = model_configurations[model_name]
+config_backbone_model['H'] = 6
+config_backbone_model['D'] = 1
+config_backbone_model['W'] = 0
+config_backbone_model['train_prop'] = 0.8
+config_backbone_model['valid_prop'] = 0.1
+config_backbone_model['standardize'] = True
+config_backbone_model['minmaxnorm'] = False
+config_backbone_model['batch_size'] = 32 # # 32
+config_backbone_model['epochs'] = 50 #5
+
+# ------------------------------------------
+
+training_save_folder = f'{inside_saved_folder}/{exp_i}' # f'K_fold_validation/training_wo_HP_tuning/{exp_i}' 
+save_path_figures = f'{current_file_path}/results/plot/{exp_i}'
+device = torch.device('cuda:0')
+add_name_save = '' #'_clipping'  # ''  # '_trial2'
+
+station_clustering = True
+for contextual_dataset_names in possible_contextual_dataset_names:
+    # assert len(contextual_dataset_names) == 1, "Only one contextual dataset at a time is allowed for this pipeline. Otherwise, update 'build_config_single_contextual.py' accordingly. "
+
+    loger = LOG()
+    SANITY_CHECKER = False # True
+    metrics = ['RMSE','MAE','MASE']
+    config_backbone_model = model_configurations[model_name]
+    config_backbone_model['batch_size'] = 32 # 150 #80
+    config_backbone_model['epochs'] = 50
+    compilation_modification['torch_compile'] = 'compile' #'compile' # 'compile' # 'compile'  # False 
+    compilation_modification['device'] = device
+    # compilation_modification['grad_clipping_norm'] = 1.0
+
+    # -----If small run if needed: 
+    if TRIVIAL_TEST:
+        config_backbone_model['epochs'] = 1 # 150 # 150 #80
+        config_backbone_model['batch_size'] = 256 # 150 #80
+        REPEAT_TRIAL  = 1
+        compilation_modification['torch_compile'] = False
+
+
+    if True:
+        # ==================================================
+        # LOAD CONFIGURATIONS TO TEST:
+        possible_contextual_kwargs = {}
+        # possible_contextual_kwargs[ds][fusion][feature_extractor]
+        print('contextual_dataset_names:\n ',contextual_dataset_names)
+        for dataset_name in contextual_dataset_names:
+            if 'weather' == dataset_name:
+                contextual_kwargs = weather_possible_contextual_kwargs['early_fusion']['repeat_t_proj']
+                possible_contextual_kwargs[dataset_name] = {'early_fusion':{'repeat_t_proj':contextual_kwargs}}
+
+            else:
+                path = f"experiences.pipeline_desag.{target_data}_pred.{dataset_name}_contextual"
+                module = importlib.import_module(path)
+                importlib.reload(module)
+                contextual_kwargs = module.get_possible_contextual_kwarg(add_name_save)     
+                possible_contextual_kwargs[dataset_name] = contextual_kwargs
+        
+
+        print('\n------------------------------- CONFIGURATIONS ----------------------------------\n')
+        print(possible_contextual_kwargs)
+        for predictive_data in possible_contextual_kwargs.keys():
+            print(f'±n--- Predictive data: {predictive_data} ---')
+            for fusion_type in possible_contextual_kwargs[predictive_data].keys():
+                print(f'  -- Fusion type: {fusion_type} ---')
+                for trial_id in possible_contextual_kwargs[predictive_data][fusion_type].keys():
+                    print(f'    {trial_id}')
+        print('\n---------------------------------------------------------------------------------\n')
+        # ==================================================
+        # LOAD CONFIG DICTIONARY: 
+        configbuilder = ConfigBuilder(target_data,contextual_dataset_names,dataset_for_coverage,model_name,horizons,freq,REPEAT_TRIAL,SANITY_CHECKER,compilation_modification)
+        dic_configs = configbuilder.build_config_single_contextual(
+                                                    dic_configs = {},
+                                                    possible_target_kwargs=possible_target_kwargs,
+                                                    config_backbone_model=config_backbone_model,
+                                                    contextual_dataset_names=contextual_dataset_names,
+                                                    possible_contextual_kwargs=possible_contextual_kwargs,
+                                                    netmob_preprocessing_kwargs=netmob_preprocessing_kwargs
+                                                    )
+
+
+        baselineconfigbuilder = BaselineConfigBuilder(target_data,contextual_dataset_names,dataset_for_coverage,model_name,horizons,freq,REPEAT_TRIAL,SANITY_CHECKER,compilation_modification,add_name_save,)
+        dic_configs = baselineconfigbuilder.build_config_single_contextual(dic_configs, possible_target_kwargs, config_backbone_model)
+
+        print(f"Total configurations to test: {len(dic_configs)}")
+        for key in dic_configs.keys():
+            print(f"  {key}")
+
+
+        # ==================================================
+        # TRAIN ALL CONFIGURATIONS WITH CONTEXTUAL AND BASELINE: 
+        loger = loop_train_save_log(loger,dic_configs,init_save_folder = training_save_folder) 
+
+
+        # ==================================================
+        # SAVE RESULTS OF EXPERIMENTS IN A .PY FILE WITH FORMAT SUITABLE FOR ANALYSIS:
+        '''
+        save the string loger.display_log() in a f"{ROOT}/experiences.pipeline_desag.results.{exp_i}.{exp_i}.py" file to be imported after
+        Expected format: 
+        results = loger.log_final
+        '''
+        # if (loger.log_final is not None) and (loger.log_final != ""):
+        if not os.path.exists(f"{ROOT}/experiences/pipeline_desag/results/{exp_i}"):
+            os.mkdir(f"{ROOT}/experiences/pipeline_desag/results/{exp_i}")
+        if not os.path.exists(f"{ROOT}/experiences/pipeline_desag/results/{exp_i}/{exp_i}.py"):
+            with open(f"{ROOT}/experiences/pipeline_desag/results/{exp_i}/{exp_i}.py",'w') as f:
+                f.write(f'results = {repr(loger.log_final)}')
+        else:
+            # If file already exists, reload module and append new results to it
+            module_path = f"experiences.pipeline_desag.results.{exp_i}.{exp_i}"
+            module = importlib.import_module(module_path)
+            importlib.reload(module)
+            results_saved = module.results
+            with open(f"{ROOT}/experiences/pipeline_desag/results/{exp_i}/{exp_i}.py",'w') as f:
+                f.write(f'results = {repr(results_saved + loger.log_final)}')
+
+    if True:
+        # ==================================================
+        # BOXPLOT FIGURE :
+        ''' 
+        Load saved results from f"{ROOT}/experiences.pipeline_desag.results.{exp_i}.{exp_i}.py"
+        '''
+        module_path = f"experiences.pipeline_desag.results.{exp_i}.{exp_i}"
+        module = importlib.import_module(module_path)
+        importlib.reload(module)
+        results_saved = module.results
+        # re._pattern = rf'{model_name}_{target_data}.*?bis'
+        re._pattern = rf"{model_name}_{target_data}_(?:{'_'.join(contextual_dataset_names)}|calendar).*?bis"
+        trials = [c[:-4] for c in list(set(re.findall(re._pattern, results_saved)))]
+
+        print('re._pattern: ',re._pattern)
+        print('trials found: ',len(trials))
+        for trial in trials:
+            print(f"   {trial}")
+
+
+        exporter = MetricExporter(results_saved, contextual_dataset_names)
+        exporter.export_all(folder_path=save_path_figures, exp_i=exp_i)
+
+
+
+        plotting_boxplot_of_trials(trials,
+                                exp_i,
+                                metrics,
+                                folder_path,
+                                target_data= target_data,
+                                model_name= model_name,
+                                dataset_names = contextual_dataset_names,
+                                save_path = save_path_figures,
+                                n_bis_range = range(1,REPEAT_TRIAL+1)
+                                )
+
+        # ==================================================
+        # DESAGREGATED VISUALISATION & SAVE OF FIGURES: 
+        ''' Pour chaque expérience `exp_i` et pour chaque horizon `horizon`: 
+            - Récupère la baseline config
+            - Récupère l'ensemble des configs dans dic_trials[exp_i]
+            - Récupère le model_args associé dans f'{exp_i}/{target_data}_{model_name}'
+            - Et pour chacune des config à tester: 
+                - Génère le desagregated plot 
+                - Print les performances
+                - Sauvegarde les figures dans f"{folder_path}/plot/{exp_i}{rainy}"
+        '''
+
+        if True:
+            # -- Get clusters for station clustering desagregated plots:
+            for topk_percent in [None,0.8]:
+                if station_clustering:
+                    # Load ds from Target Data 
+                    args_init = local_get_args(model_name,
+                                args_init = None,
+                                dataset_names=[target_data],
+                                dataset_for_coverage=dataset_for_coverage,
+                                modification = {'freq': freq,
+                                                'step_ahead': 1,
+                                                'horizon_step': 1,
+                                                'target_data': target_data,
+                                                'target_kwargs' : {target_data: possible_target_kwargs[target_data]},
+                                                })
+                    print(args_init)
+                    fold_to_evaluate=[args_init.K_fold-1]
+                    trainer,ds,model,args = load_init_model_trainer_ds(fold_to_evaluate,None,args_init,{},None)
+
+                    colmumn_name = 'Station'
+                    train_input = ds.train_input
+                    train_time_slots = ds.tensor_limits_keeper.df_verif_train.stack().unique()
+                    train_input = pd.DataFrame(train_input.numpy(),index = train_time_slots,columns = ds.spatial_unit)
+                    train_input = train_input.reindex(pd.date_range(start=train_input.index.min(),end=train_input.index.max(),freq=args_init.freq))
+                    train_input.columns.name = colmumn_name
+
+                    if topk_percent is not None: 
+                        s = train_input.sum()
+                        threshold = s.quantile(1-topk_percent)
+                        top_ids = s[s>=threshold].index.tolist()
+                        train_input = train_input[top_ids]
+                    # Get Clustering of stations from these inputs:
+                    clusterer = get_cluster(train_input,
+                                            temporal_agg='business_day',
+                                            normalisation_type ='minmax',
+                                            index= colmumn_name,
+                                            city=ds.city,
+                                            n_clusters=5, 
+                                            linkage_method='complete', 
+                                            metric='precomputed',
+                                            min_samples=2,
+                                            heatmap= True, 
+                                            daily_profile=True, 
+                                            dendrogram=True,
+                                            bool_plot = False,
+                                            folder_path= save_path_figures,
+                                            save_name = f"{exp_i}_topk{int(topk_percent*100)}_clusters" if topk_percent is not None else f"{exp_i}",
+                                            )
+                    clusters = clusterer.clusters
+                    print('Clusters: ',clusterer.clusters)
+                else:
+                    clusters = None
+
+                # -- ON RAINY & NON RAINY : 
+                get_desagregated_gains(dic_exp_to_names={
+                                            exp_i:f'{target_data}_{model_name}'
+                                            },
+                                    dic_trials = {exp_i:trials},
+                                    horizons=horizons,
+                                    comparison_on_rainy_events=True,
+                                    range_k=range(1,REPEAT_TRIAL+1),
+                                    station_clustering=station_clustering,
+                                    folder_path=f'{current_file_path}/results',
+                                    save_bool=True,
+                                    heatmap= True,
+                                    daily_profile=True,
+                                    dendrogram=True,
+                                    dataset_names =contextual_dataset_names,
+                                    bool_plot = False,
+                                    clusters = clusters,
+                                    topk_percent = topk_percent
+                                    )
